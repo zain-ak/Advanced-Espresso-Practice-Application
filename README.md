26b25b06f2996e695c8312e3513048da3eb884b4
Zain Khalid <zak565@uowmail.edu.au>
2019-03-20 16:46:24 -0400

    IdeasActivityTest.kt: - Adjusted activtyRule for intents - Tested all possible intents for the application: missing, punny,   famous, popular (Working)
    Updated README.md

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 A Kotlin-based Android Application for learning some of the more advanced concepts of the Espresso framework for UI Testing.
 <br>
 <img src="https://camo.githubusercontent.com/737e7380383ffcd2f3b9bf55c678f3b368feb730/68747470733a2f2f6c68352e676f6f676c6575736572636f6e74656e742e636f6d2f2d453259504c6c56416c30552f564a556350726756432d492f414141414141414147464d2f416b715a6e354e387272632f773839302d68313030392f657370726573736f5f6c6f636b75702e706e67" height="200" width="175" />
+<br>
+When working with intents in test, `ActivityTestRule` changes, it'll now take in three parameters:
+ - _The class_ that was being used previously, this stays the same;
+ - `touchMode`, essentially indicates whether the last interaction performed was with the touch screen. This will be set to _true_;
+ - `launchActivity`, this is set to _false_ here because the activity is now not the default one as it was previously. Now you're going to be passing in an intent.
+
+With the initialization changed for `ActivityTestRule`, you'll now have to explicity call the activity before you can start performing tests on it. This will be done using the `ActivityTestRule.launchActivity()` method.
+
+Before you do this, you can now create an intent in the test function, and add data to it like you would in a normal activty with the code ```java
+val intent = Intent()
+intent.putExtra(_data_)
+```
+
+
--- app/src/androidTest/java/android/learning/advancedespressopracticeapplication/IdeasActivityTest.kt
+++ app/src/androidTest/java/android/learning/advancedespressopracticeapplication/IdeasActivityTest.kt
@@ -1,5 +1,6 @@
 package android.learning.advancedespressopracticeapplication
 
+import android.content.Intent
 import android.support.test.espresso.Espresso.onView
 import android.support.test.espresso.assertion.ViewAssertions.matches
 import android.support.test.espresso.matcher.ViewMatchers.withId
@@ -14,12 +15,46 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class)
 class IdeasActivityTest {
     @Rule @JvmField
-    var activityRule = ActivityTestRule(IdeasActivity::class.java)
+    //var activityRule = ActivityTestRule(IdeasActivity::class.java) used when launching activity directly
+
+    //The below activity Rule is used for passing in intents, it has two additional parameters
+    var activityRule = ActivityTestRule(IdeasActivity::class.java, true, false)
 
     @Test
     fun noTheme() {
+        activityRule.launchActivity(null) //has to be explicitly called since launchActivty == false, above.
         onView(withId(R.id.theme))
             .check(matches(withText(R.string.missing_theme)))
+    }
+
+    @Test
+    fun punny() {
+        val intent = Intent()
+        intent.putExtra(IdeasActivity.KEY_THEME, "Punny")
+        activityRule.launchActivity(intent)
+        onView(withId(R.id.theme))
+            .check(matches(withText(R.string.theme_punny)))
+
+    }
+
+    @Test
+    fun popular() {
+        val intent = Intent()
+        intent.putExtra(IdeasActivity.KEY_THEME, "Popular")
+        activityRule.launchActivity(intent)
+        onView(withId(R.id.theme))
+            .check(matches(withText(R.string.theme_popular)))
+
+    }
+
+    @Test
+    fun famous() {
+        val intent = Intent()
+        intent.putExtra(IdeasActivity.KEY_THEME, "Famous")
+        activityRule.launchActivity(intent)
+        onView(withId(R.id.theme))
+            .check(matches(withText(R.string.theme_famous)))
+
     }
 
 
